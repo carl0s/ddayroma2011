1c8f5db5fcd3c10b9971b0471b595bbbc3726151
c <carlo@nois3lab.it>
2011-10-23 23:53:32 +0200

    kinda fixed session, though it's still missing of pieces

---
+++ sites/all/themes/dday/templates/node--session.tpl.php
@@ -0,0 +1,13 @@
+<article class="session">
+  <div class="grid-3 detail alpha">
+    <span class="timeslot">h 9:00</span>
+    <h4>difficoltà</h4>
+    <span class="difficulty">****</span>
+    <h4>da sapere</h4>
+    <div class="helpful"></div>
+  </div>
+  <div class="grid-13 omega">
+    <h1 id="page-title"><?php print $title; ?></h2>    
+    <?php print render($content['body']); ?>
+  </div>
+</article>
