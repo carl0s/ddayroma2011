0df743fa7c0f891dbad09364ad9fb37d53064366
carl0s_ <carlo@nois3lab.it>
2011-10-24 17:14:26 +0200

    clearing right way

--- sites/all/themes/dday/templates/node--session.tpl.php
+++ sites/all/themes/dday/templates/node--session.tpl.php
@@ -1,4 +1,4 @@
-<article class="session clear-block">
+<article class="session clearfix">
   <div class="grid-3 detail alpha">
     <span class="timeslot"> <?php print $content['field_session_timeslot'][0]['#title'] ?> </span>
     <h4>difficoltà</h4>
