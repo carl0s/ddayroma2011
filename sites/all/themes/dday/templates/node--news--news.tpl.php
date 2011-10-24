93fe5f78846daac66d502baa036e526470197135
carl0s_ <carlo@nois3lab.it>
2011-10-24 15:06:22 +0200

    added images for session boxes

--- sites/all/themes/dday/templates/node--news--news.tpl.php
+++ sites/all/themes/dday/templates/node--news--news.tpl.php
@@ -1,5 +1,5 @@
 <article class="news">
   <h2><a href="<?php $node_url ?>" title="<?php print $title?>"><?php print $title; ?></a></h2>
-  <h3><?php print $created; ?></h3>
+  <h3><?php print format_date($created, $type = 'medium', $format = '', $timezone = NULL, $langcode = NULL); ?></h3>
   <?php print render($content['body']); ?>
 </article>
