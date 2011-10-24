38ca43bd919d241f445a2429e1cf33ead1c3f541
c <carlo@nois3lab.it>
2011-10-25 01:34:39 +0200

    adding templates, this practice sucks, tho :(

---
+++ sites/all/themes/dday/templates/views-view-field--sponsor--attachment-2--field-sponsor-logo.tpl.php
@@ -0,0 +1,7 @@
+<?php
+    if ($row->_field_data['nid']['entity']->field_sponsor_fake_real['und'][0]['value'] == 'fake') {
+        print '<a href="/diventa-sponsor"><img typeof="foaf:Image" src="' . base_path() . '/sites/default/files/styles/sponsor_silver/public/session.jpg" alt="" /></a>';
+    } else {
+      print $output;
+    }
+?>
