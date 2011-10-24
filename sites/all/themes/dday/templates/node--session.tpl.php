7cdafe5f969fae33525e93b932549865b72f5b29
Paolo Mainardi <paolomainardi@gmail.com>
2011-10-24 04:05:50 +0200

    Implementato tag session, tag cose da sapere, implementata pagina sponsor, close #4

--- sites/all/themes/dday/templates/node--session.tpl.php
+++ sites/all/themes/dday/templates/node--session.tpl.php
@@ -1,13 +1,25 @@
 <article class="session">
   <div class="grid-3 detail alpha">
-    <span class="timeslot">h 9:00</span>
+    <span class="timeslot"> <?php print $content['field_session_timeslot'][0]['#title'] ?> </span>
     <h4>difficoltà</h4>
-    <span class="difficulty">****</span>
-    <h4>da sapere</h4>
-    <div class="helpful"></div>
+    <?php if ($content['field_session_experience_level']) : ?>
+      <span class="difficulty <?php print ($experience_class ? $experience_class : '') ?> ">
+        <?php print drupal_render($content['field_session_experience_level']); ?>
+      </span>
+    <?php endif; ?>
+    <?php if ($content['field_session_tags']) : ?>
+      <h4>da sapere</h4>
+      <div class="helpful">
+        <?php print drupal_render($content['field_session_to_know_tags']) ?>
+      </div>
+    <?php endif; ?>
   </div>
   <div class="grid-13 omega">
-    <h1 id="page-title"><?php print $title; ?></h2>    
+    <?php if ($page) :?>
+      <h1 id="page-title"><?php print $title; ?></h1>    
+    <?php else : ?>
+      <h2 id='page-title'><?php print l($title, 'node/'.$node->nid) ?> </h2>
+    <?php endif; ?>
     <?php print render($content['body']); ?>
   </div>
 </article>
