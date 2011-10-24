5327f5edd2afb929661211f619b097662914a5d2
carl0s_ <carlo@nois3lab.it>
2011-10-24 14:01:20 +0200

    added ignore for dumps

--- sites/all/themes/dday/templates/node--news--news.tpl.php
+++ sites/all/themes/dday/templates/node--news--news.tpl.php
@@ -1,5 +1,5 @@
 <article class="news">
   <h2><a href="<?php $node_url ?>" title="<?php print $title?>"><?php print $title; ?></a></h2>
-  <h3><?php print $submitted; ?></h3>
+  <h3><?php print $created; ?></h3>
   <?php print render($content['body']); ?>
 </article>
