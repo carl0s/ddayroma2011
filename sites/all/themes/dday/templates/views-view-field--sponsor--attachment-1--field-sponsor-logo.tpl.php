8ee9751307ea4a5f302e585277edafffda2e3d8b
Paolo Mainardi <paolomainardi@gmail.com>
2011-10-25 02:00:47 +0200

    fixed images

--- sites/all/themes/dday/templates/views-view-field--sponsor--attachment-1--field-sponsor-logo.tpl.php
+++ sites/all/themes/dday/templates/views-view-field--sponsor--attachment-1--field-sponsor-logo.tpl.php
@@ -1,6 +1,6 @@
 <?php
     if ($row->_field_data['nid']['entity']->field_sponsor_fake_real['und'][0]['value'] == 'fake') {
-        print '<a href="/diventa-sponsor"><img typeof="foaf:Image" src="' . base_path() . '/sites/default/files/styles/sponsor_gold/public/session.jpg" alt="" /></a>';
+        print '<a href="/diventa-sponsor"><img typeof="foaf:Image" src="' . base_path() . 'sites/all/themes/dday/images/gold.png" alt="" /></a>';
     } else {
       print $output;
     }
