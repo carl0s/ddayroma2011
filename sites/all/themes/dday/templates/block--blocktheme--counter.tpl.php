7ae41cc8211a9ffd9cd48a4e45dc3c3ce5cf5617
carl0s_ <carlo@nois3lab.it>
2011-10-27 17:26:27 +0200

    merged eugenio

--- sites/all/themes/dday/templates/block--blocktheme--counter.tpl.php
+++ sites/all/themes/dday/templates/block--blocktheme--counter.tpl.php
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block block user-count-block block-<?php print $block->module ?>">
+=======
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="clear-block user-count-block block block-<?php print $block->module ?>">
+>>>>>>> dday2011_eugenio
   <?php print theme('render_drupalday_system_feature_user_count'); ?>
   <div class="content"><?php print $content ?></div>
 </div>
