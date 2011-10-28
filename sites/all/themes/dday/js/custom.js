ec0398acec94d3920ba5cc4ff93f981884c24a13
c <carlo@nois3lab.it>
2011-10-28 09:07:41 +0200

    fixed interaction for bottom blocks, impressive.

--- sites/all/themes/dday/js/custom.js
+++ sites/all/themes/dday/js/custom.js
@@ -11,13 +11,38 @@
         $(this).addClass('selected');
       });
       
-      $('#mydrupalday').live('click',function() {
+      $('#mydrupalday').click(function() {
         $('.my-dday-container').css('display','block');
-        
         return false;
       })
       
      // $('.scrollable').scrollable({ circular:true, speed: 800 }).autoscroll(13000).navigator();
+     $('#region-user-first ul.menu li a').hover(
+        function() {
+          $('#region-preface-first .region-inner').addClass('selected');
+        },
+        function() {
+          $('#region-preface-first .region-inner').removeClass('selected');
+        }
+     )
+
+     $('#region-user-second ul.menu li a').hover(
+        function() {
+          $('#region-preface-second .region-inner').addClass('selected');
+        },
+        function() {
+          $('#region-preface-second .region-inner').removeClass('selected');
+        }
+     )
+     
+     $('#region-user-third ul.menu li a').hover(
+        function() {
+          $('#region-preface-third .region-inner').addClass('selected');
+        },
+        function() {
+          $('#region-preface-third .region-inner').removeClass('selected');
+        }
+     )
 
     }
   };
