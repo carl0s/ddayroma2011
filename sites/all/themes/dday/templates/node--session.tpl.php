9c875dd8f0c8d629c0bdb139bf5401d78b51c684
Paolo Mainardi <paolomainardi@gmail.com>
2011-10-31 21:53:46 +0100

    fix session node

--- sites/all/themes/dday/templates/node--session.tpl.php
+++ sites/all/themes/dday/templates/node--session.tpl.php
@@ -3,14 +3,14 @@
     <span class="timeslot"> <?php print $content['field_session_timeslot'][0]['#title'] ?> </span>
     <h4>difficoltà</h4>
     <?php if ($content['field_session_experience_level']) : ?>
-      <span class="difficulty <?php print ($experience_class ? $experience_class : '') ?> ">
+      <span class="difficulty<?php print ($experience_class ? $experience_class : '') ?> ">
         <?php print drupal_render($content['field_session_experience_level']); ?>
       </span>
     <?php endif; ?>
-    <?php if ($content['field_user_interest']) : ?>
+    <?php if ($content['field_session_to_know_tags']) : ?>
       <h4>da sapere</h4>
       <div class="helpful">
-        <?php print drupal_render($content['field_user_interest']) ?>
+        <?php print drupal_render($content['field_session_to_know_tags']) ?>
       </div>
     <?php endif; ?>
   </div>
@@ -20,11 +20,11 @@
     <?php else : ?>
       <h2 id='page-title'><?php print l($title, 'node/'.$node->nid) ?> </h2>
     <?php endif; ?>
-    <span class="username"> di <?php print theme('username', array('account' => user_load(array('uid' => $node->uid)))) ?> </span>
+    <span class="username"> di <?php print theme('username', array('account' => $account)) ?> </span>
     
-    <?php if ($content['field_user_skills']) : ?>
+    <?php if ($content['field_session_tags']) : ?>
       <div class="tags">
-        <?php print drupal_render($content['field_user_skills']) ?>
+        <?php print drupal_render($content['field_session_tags']) ?>
       </div>
     <?php endif; ?>
     
