295d23e668823212111c031744c30fa0213ed9b3
Paolo Mainardi <paolomainardi@gmail.com>
2011-11-22 23:38:45 +0100

    update sessions

--- sites/all/themes/dday/templates/node--session.tpl.php
+++ sites/all/themes/dday/templates/node--session.tpl.php
@@ -2,12 +2,12 @@
   <div class="grid-3 detail alpha">
     <span class="timeslot"> <?php print $content['field_session_timeslot'][0]['#title'] ?> </span>
     <h4>difficoltà</h4>
-    <?php if ($content['field_session_experience_level']) : ?>
+    <?php if (isset($content['field_session_experience_level'])) : ?>
       <span class="difficulty<?php print ($experience_class ? $experience_class : '') ?> ">
         <?php print drupal_render($content['field_session_experience_level']); ?>
       </span>
     <?php endif; ?>
-    <?php if ($content['field_session_to_know_tags']) : ?>
+    <?php if (isset($content['field_session_to_know_tags'])) : ?>
       <h4>da sapere</h4>
       <div class="helpful">
         <?php print drupal_render($content['field_session_to_know_tags']) ?>
@@ -20,15 +20,19 @@
     <?php else : ?>
       <h2 id='page-title'><?php print l($title, 'node/'.$node->nid) ?> </h2>
     <?php endif; ?>
-    <span class="username"> di <?php print theme('username', array('account' => $account)) ?> </span>
     
-    <?php if ($content['field_session_tags']) : ?>
+    <div class="speakers"> <?php print drupal_render($content['field_session_presenters']) ?> </div>
+    
+    <?php if (isset($content['field_session_tags'])) : ?>
       <div class="tags">
         <?php print drupal_render($content['field_session_tags']) ?>
       </div>
     <?php endif; ?>
     
+    <?php print render($content['links']['flag']) ?>
     
-    <?php print render($content['body']); ?>
+    <div class="session-content">
+      <?php print render($content['body']); ?>
+    </div>
   </div>
 </article>
