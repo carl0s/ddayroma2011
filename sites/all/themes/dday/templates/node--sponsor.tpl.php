8deebead125154513a8e6d4d30dd1366c0aa1537
Paolo Mainardi <paolomainardi@gmail.com>
2011-11-23 02:18:10 +0100

    sponsor field

--- sites/all/themes/dday/templates/node--sponsor.tpl.php
+++ sites/all/themes/dday/templates/node--sponsor.tpl.php
@@ -3,6 +3,7 @@
     <figure>
       <?php print drupal_render($content['field_sponsor_logo']); ?>
     </figure>
+    <?php print drupal_render($content['field_sponsor_session']); ?>
   </aside>
   <section id="content" class="grid-8 omega">
     <?php print drupal_render($content['field_sponsor_website']); ?>
